d17e3321f1d2f4e339b80715c26b154dce57a2cc
Andrey <andrman560@gmail.com>
2022-11-12 21:53:46 +0300

    Fix registration

--- webnotes/webnotes/settings.py
+++ webnotes/webnotes/settings.py
@@ -116,18 +116,13 @@ USE_I18N = True
 
 USE_TZ = False
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
+
+#STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
